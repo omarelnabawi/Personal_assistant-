be2ebddcb500f71bf7704a9177e24fe6c2a32a41
omarelnabawi <omarelnabawi@gmail.com>
2026-09-16 19:55:16 +0300

    Add settings module for configuration management using Pydantic

---
+++ src/config/settings.py
@@ -0,0 +1,25 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import field_validator
+
+class Settings(BaseSettings):
+    GROQ_API_KEY: str
+    GOOGLE_API_KEY: str
+    GROQ_FALLBACK_MODELS: str 
+    GEMINI_FALLBACK_MODEL: str 
+    Model_TEMPERATURE: float
+    Model_MAX_TOKENS: int 
+    Model_TOP_P: float 
+    Model_FREQUENCY_PENALTY: float 
+    Model_PRESENCE_PENALTY: float 
+    MAX_MESSAGES: int
+    KEEP_RECENT: int
+    local_user: str 
+
+    model_config = SettingsConfigDict(env_file=".env",ignore_extra=True)
+
+    @property
+    def fallback_models_list(self) -> list[str]:
+        return [m.strip() for m in self.GROQ_FALLBACK_MODELS.split(",")]
+
+def get_settings():
+    return Settings()
