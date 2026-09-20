4d9656a0729ca8710a3b60a0ad5652c4b6e9b5a4
omarelnabawi <omarelnabawi@gmail.com>
2026-09-20 16:25:21 +0300

    Refactor model_router.py to enhance logging and error handling in chat model functions

--- src/controllers/model_router.py
+++ src/controllers/model_router.py
@@ -2,13 +2,12 @@ from langchain_groq import ChatGroq
 from groq import BadRequestError, RateLimitError, APIError
 from langchain_google_genai import ChatGoogleGenerativeAI
 #---------------------------------
-#from helper import get_settings
+from helper import logger
 #---------------------------------
 #settings = get_settings()
 #FALLBACK_MODELS = settings.fallback_models_list
 
-
-def _try_groq(settings, formatted_prompt,tools=None):
+def _try_groq(settings, formatted_prompt,tools=None,stage="unknown"):
     last_error = None
     formatted_prompt = sanitize_messages(formatted_prompt)
     for model_name in settings.fallback_models_list:
@@ -24,15 +23,17 @@ def _try_groq(settings, formatted_prompt,tools=None):
                 chat = chat.bind_tools(tools)
 
             response = chat.invoke(formatted_prompt)
-            print(f"✅ اشتغل بالموديل: {model_name} (Groq)")
+            logger.info(f"[{stage}] Model Run successfuly : {model_name} (Groq) ✅")
+            logger.info(f"Ratio of resoning from the output tokens:\n{response.usage_metadata['output_token_details']['reasoning']/response.usage_metadata['output_tokens']*100}%"  )
+            logger.debug(f"response:\n**{response}**")
             return response
         except (BadRequestError, RateLimitError, APIError) as e:
-            print(f"⚠️ فشل {model_name}: {e}")
+            logger.warning(f"[{stage}] فشل {model_name}: {e}")
             last_error = e
             continue
-    raise RuntimeError(f"كل موديلات Groq فشلت. آخر خطأ: {last_error}")
+    raise RuntimeError(f"[{stage}] problem in Gtoq ❌: {last_error}")
 
-def _try_gemini(settings, formatted_prompt,tools=None):
+def _try_gemini(settings, formatted_prompt,tools=None,stage="unknown"):
     formatted_prompt = sanitize_messages(formatted_prompt)
     chat = ChatGoogleGenerativeAI(
         model=settings.GEMINI_FALLBACK_MODEL,
@@ -43,15 +44,15 @@ def _try_gemini(settings, formatted_prompt,tools=None):
         chat = chat.bind_tools(tools)
     
     response = chat.invoke(formatted_prompt)
-    print(f"✅ اشتغل بموديل: {settings.GEMINI_FALLBACK_MODEL} ")
+    logger.info(f"[{stage}] Model Run successfuly : {settings.GEMINI_FALLBACK_MODEL} ✅")
     return response
 
-def get_working_chat(settings, formatted_prompt,tools=None):
+def get_working_chat(settings, formatted_prompt,tools=None,stage="unknown"):
     try:
-        return _try_groq(settings, formatted_prompt,tools)
+        return _try_groq(settings, formatted_prompt,tools,stage)
     except RuntimeError as groq_error:
-        print(f"🔄 Groq فشلت بالكامل، بنحوّل لـ Gemini... ({groq_error})")
-        return _try_gemini(settings, formatted_prompt)
+        logger.warning(f"[{stage}] Groq فشلت بالكامل، بنحوّل لـ Gemini... ({groq_error})")
+        return _try_gemini(settings, formatted_prompt,stage)
 
 def extract_answer(response) -> str:
     content = response.content
@@ -60,7 +61,7 @@ def extract_answer(response) -> str:
     return str(content)   # fallback نادر جدًا هيحصل
 
 
-def get_working_chat_structured(settings, formatted_prompt, schema):  
+def get_working_chat_structured(settings, formatted_prompt, schema,stage="extract New Data"):  
     last_error = None
     formatted_prompt = sanitize_messages(formatted_prompt)
     for model_name in settings.fallback_models_list:
@@ -71,17 +72,17 @@ def get_working_chat_structured(settings, formatted_prompt, schema):
                 max_tokens=settings.Model_MAX_TOKENS,
                 api_key=settings.GROQ_API_KEY.get_secret_value(),
                 reasoning_effort="low",
-                reasoning_format="hidden",
+                reasoning_format="hidden"
             )
             structured_chat = chat.with_structured_output(schema,method="json_schema")
             response = structured_chat.invoke(formatted_prompt)
-            print(f"✅ Data extraction success : {model_name} (Groq)")
+            logger.info(f"[{stage}] Model Extraction success : {model_name} (Groq) ✅")
             return response
         except (BadRequestError, RateLimitError, APIError) as e:
-            print(f"⚠️ faield extraction:  {model_name}: {e}")
+            logger.warning(f"[{stage}] فشل {model_name}: {e}")
             last_error = e
             continue
-    raise RuntimeError(f"كل موديلات Groq فشلت في الاستخراج المنظم. آخر خطأ: {last_error}")
+    raise RuntimeError(f"[{stage}] كل موديلات Groq فشلت في الاستخراج المنظم. آخر خطأ: {last_error}")
 
 def clean_text(text):
     if isinstance(text, str):
